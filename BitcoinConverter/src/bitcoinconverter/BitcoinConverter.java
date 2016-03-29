aa1dc95a8d63f81985cf3867c8565bf0dbe05be0
Rishabh <rishabhshah28@gmail.com>
2016-03-29 09:12:47 -0400

    initial commit

---
+++ BitcoinConverter/src/bitcoinconverter/BitcoinConverter.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bitcoinconverter;
+
+/**
+ *
+ * @author rishabh
+ */
+
+import java.util.Scanner;
+        
+public class BitcoinConverter {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner user_input = new Scanner( System.in);
+        
+        String space = " ";
+        int DoContinue = 1;
+        String DoContinueString;
+        String UserInputString;
+        
+        String BTCValueString;
+        System.out.print("Enter what one bitcoin is worth in US Dollars: ");
+        BTCValueString = user_input.next();
+        float BTCValueFloat = Float.valueOf(BTCValueString);
+        
+        while (DoContinue == 1)  {
+            String UserChoice;
+            System.out.print("Enter 'a' if you would like to convert from BTC to USD and 'b' if you would like to convert from USD to BTC: ");
+            UserChoice = user_input.next();
+
+            String BTCtoUSD = "a";
+            String USDtoBTC = "b";
+
+            if (UserChoice.equals(BTCtoUSD))  {
+                System.out.print("Enter the BTC amount to convert to USD: ");
+                UserInputString = user_input.next();
+                float UserInputFloat = Float.valueOf(UserInputString);
+                float Answer = (UserInputFloat * BTCValueFloat);
+                System.out.println("The amount of USD is: " + Answer);
+                
+                System.out.print("Would you like to try again? (yes/no): ");
+                DoContinueString = user_input.next();
+                if (DoContinueString.equals("yes"))  {
+                    DoContinue = 1;
+                }
+                else if (DoContinueString.equals("no")) {
+                    DoContinue = 0;
+                }  
+            }
+
+            else if (UserChoice.equals(USDtoBTC))  { 
+                System.out.print("Enter the USD amount to convert to BTC: ");
+                UserInputString = user_input.next();
+                float UserInputFloat = Float.valueOf(UserInputString);
+                float Answer = (UserInputFloat / BTCValueFloat);
+                System.out.println("The amount of BTC is: " + Answer);
+                
+                System.out.print("Would you like to try again? (yes/no): ");
+                DoContinueString = user_input.next();
+                if (DoContinueString.equals("yes"))  {
+                    DoContinue = 1;
+                }
+                else if (DoContinueString.equals("no")) {
+                    DoContinue = 0;
+                }
+            }
+
+            else    {
+                System.out.println("You entered something that does not compute. Please try again.");
+            }
+        }
+    }
+}
